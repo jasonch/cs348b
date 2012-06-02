c5be5e0fbb95e861acaf921d6dd80262d025c336
Jason Chen <jasonch@jasonch-PC.(none)>
2012-06-02 03:36:31 -0700

    adding feedback to path tool

--- app/assets/javascripts/tools/path.js
+++ app/assets/javascripts/tools/path.js
@@ -1,16 +1,23 @@
 function Path(canvasCallback) {
+  $('#pathCurPoint').remove();
+  $('#pathLastPoint').remove();
   this.canvas = canvasCallback;
   this.firstPoint = true;
   this.canvasContext().beginPath();
+  //this.curPoint = $("<div>").attr('id','pathCurPoint').width(10).height(10).css("background-color", "gray").css("position", "absolute").css('z-index', 1000).appendTo($("body")).hide();
+  this.lastPoint = $("<div>").attr('id','pathLastPoint').width(10).height(10).css("background-color", "black").css("position", "absolute").css('z-index', 1000).appendTo($("body")).hide();
+
+  $(this.canvas()).css('cursor', 'crosshair');
 }
 
 Path.prototype = new Tool("path", "Path");
 
 
 Path.prototype.mouseDown = function(event) {
+    event.preventDefault();
     this.canvas().changed = true;
 
-    console.log(this);
+    $(this.lastPoint).offset({top: event.pageY-5, left: event.pageX-5}).show();
     this.canvasContext().strokeStyle = EditorStates.stroke;
     this.canvasContext().lineWidth = EditorStates.strokeWidth;
 
